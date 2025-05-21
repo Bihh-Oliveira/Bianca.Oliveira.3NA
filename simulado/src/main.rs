d6c03970760602a61e113a3e20609738456b2291
Bihh-Oliveira <bibiolly2006@gmail.com>
2025-05-21 23:35:57 +0000

    simulado

--- simulado/src/main.rs
+++ simulado/src/main.rs
@@ -1,3 +1,23 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec = Vec::new();
+    
+    let mut rng = rand::thread_rng();
+    
+
+    for _ in 0..20 {
+        vec.push(rng.gen_range(1..=100));
+    }
+
+    // vetor original
+    println!("Vetor original: {:?}", vec);
+
+    // Ordena o vetor
+    vec.sort();
+
+    // vetor ordenado
+    println!("Vetor ordenado: {:?}", vec);
 }
+
+
